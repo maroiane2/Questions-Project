2495b8b50fc67b815cdf945c850b6c37b9852228
marouan <devopscode9@gmail.com>
2023-03-28 18:37:17 +0100

    finsh

--- README.md
+++ README.md
@@ -7,20 +7,23 @@
 ### 05 - Fatch Number Of Question Frome JSON Object
 ### 06 - Show Questions Count + Qoestions Categrory
 ### 07 - Show The Number Of Bullets and Wrong Answers
+### 10 - Fetch The Questions From The JSON Object
+### 11 - Show First Qoustion
+### 08 - Set The Nummbrt For The Right and Wrong Answers
+### 09 - Create Countdown Timer
+### 12 - Check Answer and Hide Question Then Show Next One
+### 13 - If The Time End Mark The Question As Wrong and Show Next Question
+### 14 - add Class on Bullet After Answeing The Question
+### 15 - If Questions Finished Show the REsult
 
 <br>
 <br>
 
 ## InProgress
 
-### 08 - Set The Nummbrt For The Right and Wrong Answers
-### 09 - Create Countdown Timer
-### 10 - Fetch The Questions From The JSON Object
-### 11 - Show First Qoustion
-### 12 - Check Answer and Hide Question Then Show Next One
-### 13 - If The Time End Mark The Question As Wrong and Show Next Question
-### 14 - add Class on Bullet After Answeing The Question
-### 15 - If Questions Finished Show the REsult
+### X
+### X
+### X
 
 <br>
 <br>
--- js/scrept.js
+++ js/scrept.js
@@ -1,13 +1,19 @@
 // Select Elements
 let countSpan = document.querySelector(" .count span");
+let bulletsElment = document.querySelector(".bullets")
 let bulletspanContainer = document.querySelector(".bullets .spans ");
 let quizArea = document.querySelector(" .Quest")
 let answersArea = document.querySelector(" .answer_area")
-
+let submitButton = document.querySelector(".boxAll .Question button")
+let timeer = document.querySelector(".time")
+let resultsContainer = document.querySelector(".results")
+let countdownElement = document.querySelector(".time")
 
 
 // Set Options
 let currentIndex = 0;
+let RightAnswer = 0;
+let countdownInterval;
 
 function getQuestions() {
 
@@ -22,6 +28,38 @@ function getQuestions() {
 
             // Add Question Data
             addQuestionData(questionsObject[currentIndex], qCount);
+
+            // Start CountDown
+            countdown(5, qCount);
+
+            // Click on Submit 
+            submitButton.onclick = () => {
+                // Get figh answer
+                let theRightAnswer = questionsObject[currentIndex].right_answer;
+
+                // Increase index
+                currentIndex++;
+
+                // Check the Answer
+                checkAnswer(theRightAnswer, qCount);
+
+                // remove Previous QUestion
+                quizArea.innerHTML = "";
+                answersArea.innerHTML = "";
+
+                // Add Question Data
+                addQuestionData(questionsObject[currentIndex], qCount);
+
+                // handle bullets Class
+                handleBullets();
+
+                // Start CountDown
+                clearInterval(countdownInterval);
+                countdown(5, qCount);
+
+                // show Results
+                showResults(qCount);
+            };
         }
     };
 
@@ -36,86 +74,152 @@ function createBullets(num) {
     countSpan.innerHTML = num;
 
     // Create Spans
-    for (let i = 0 ; i < num; i++) {
+    for (let i = 0; i < num; i++) {
 
-    // Create Spans
+        // Create Spans
         let theBullet = document.createElement("span");
 
         // Check if Its First Span
-        if (i === 0 ) {
+        if (i === 0) {
             theBullet.className = "on";
         }
 
-    // Append Bullets To Main Bullet Container
-    bulletspanContainer.appendChild(theBullet);
+        // Append Bullets To Main Bullet Container
+        bulletspanContainer.appendChild(theBullet);
     }
 }
 
 function addQuestionData(obj, count) {
-    //Create H2 Question title
-    let questionstitle = document.createElement("h2");
+    if (currentIndex < count) {
+        //Create H2 Question title
+        let questionstitle = document.createElement("h2");
 
-    // Create Question Text
-    let questionsText = document.createTextNode(obj['title'])
+        // Create Question Text
+        let questionsText = document.createTextNode(obj['title'])
 
-    // append Text to h2
-    questionstitle.appendChild(questionsText);
+        // append Text to h2
+        questionstitle.appendChild(questionsText);
 
-    // append the h2 to the Quiz Area
-    quizArea.appendChild(questionstitle);
+        // append the h2 to the Quiz Area
+        quizArea.appendChild(questionstitle);
 
-    //Create The Answers
-    for (let i = 1; i <= 4; i++) {
-        // Create Main Answer div
-        let mainDiv = document.createElement("div");
+        //Create The Answers
+        for (let i = 1; i <= 4; i++) {
+            // Create Main Answer div
+            let mainDiv = document.createElement("div");
 
-        // add class to main Div
-        mainDiv.className = 'answer';
+            // add class to main Div
+            mainDiv.className = 'answer';
 
+            // create A B C D
+            let h2ABCD = document.createElement("h2");
+            const letter = String.fromCharCode(64 + i);
+            h2ABCD.innerHTML = letter;
 
-        // create A B C D
-        let h2ABCD = document.createElement("h2");
+            //Create Radio Input
+            let radioInput = document.createElement("input");
 
-        const letter = String.fromCharCode(64 + i);
-        h2ABCD.innerHTML = letter;
+            // add type + name + Id + DAta-attribute
+            radioInput.name = 'questions';
+            radioInput.type = 'radio';
+            radioInput.id = `answer_${i}`;
+            radioInput.dataset.answer = obj[`answer_${i}`];
 
+            // // Make first Option selcted
+            // if (i === 1) {
+            //     radioInput.checked = true;///////////////////////////
+            // }
 
+            // create label
+            let theLabal = document.createElement("label");
 
+            // add for Attribute
+            theLabal.htmlFor = `answer_${i}`;
 
-        //Create Radio Input
-        let radioInput = document.createElement("input");
+            // create label Text
+            let theLabalText = document.createTextNode(obj[`answer_${i}`]);
 
-        // add type + name + Id + DAta-attribute
-        radioInput.name = 'questions';
-        radioInput.type = 'radio';
-        radioInput.id = `answer_${i}`;
-        radioInput.dataset.answer = obj[`answer_${i}`];
+            //add the Text to label
+            theLabal.appendChild(theLabalText)
 
-        // Make first Option selcted
-        if (i === 1) {
-            radioInput.checked = true;
+            // add input + label to main div
+            mainDiv.appendChild(h2ABCD);
+            mainDiv.appendChild(radioInput);
+            mainDiv.appendChild(theLabal);
+
+            // append all divs to answer area
+            answersArea.appendChild(mainDiv)
         }
+    }
+}
 
-        // create label
-        let theLabal = document.createElement("label");
+function checkAnswer(rAnswer, count) {
+    let answers = document.getElementsByName("questions")
+    let theChoosenAnswer;
 
-        // add for Attribute
-        theLabal.htmlFor = `answer_${i}`;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i].checked) {
+            theChoosenAnswer = answers[i].dataset.answer;
+        };
 
-        // create label Text
-        let theLabalText = document.createTextNode(obj[`answer_${i}`]);
+    };
 
-        //add the Text to label
-        theLabal.appendChild(theLabalText)
+    if (rAnswer === theChoosenAnswer) {
+        RightAnswer++;
+        console.log("good Answer");
+    }
+};
 
-        // add input + label to main div
-        mainDiv.appendChild(h2ABCD);
-        mainDiv.appendChild(radioInput);
-        mainDiv.appendChild(theLabal);
-        
+function handleBullets() {
+    let bulletsSpans = document.querySelectorAll(" .bullets .spans span");
+    let arrayOfSpans = Array.from(bulletsSpans);
+    arrayOfSpans.forEach((span, index) => {
+        if (currentIndex === index) {
+            span.className = "on";
+        }
+    })
+}
 
+function showResults(count) {
+    let theResults;
+    if (currentIndex === count) {
+        quizArea.remove();
+        answersArea.remove();
+        submitButton.remove();
+        bulletsElment.remove();
+        timeer.remove();
 
-        // append all divs to answer area
-        answersArea.appendChild(mainDiv)
+        if (RightAnswer > count / 2 && RightAnswer < count) {
+            theResults = `<span class="good">Good</span>, ${RightAnswer} from ${count} this Is Good.`;
+        } else if (RightAnswer === count) {
+            theResults = `<span class="perfect">perfect</span>, All Answers Is Good.`;
+        } else {
+            theResults = `<span class="bad">bad</span>, ${RightAnswer} from ${count} this Is not Good.`;
+        }
+        // resultsContainer.styl.display= "au";
+        resultsContainer.innerHTML = theResults;
+        resultsContainer.style.margin = '227px';
+        resultsContainer.style.border = '#000 solid px';
+    }
+}
+
+function countdown(duration, count) {
+    if (currentIndex < count) {
+        let minutes, seconds;
+        countdownInterval = setInterval(function () {
+            minutes = parseInt(duration / 60);
+            seconds = parseInt(duration % 60);
+
+            minutes = minutes < 10 ? `0${minutes}` : minutes;
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+            countdownElement.innerHTML = `${minutes}:${seconds}`;
+
+            if (--duration < 0) {
+                clearInterval(countdownInterval);
+                submitButton.click()
+            }
+
+        }, 1000);
     }
 }
